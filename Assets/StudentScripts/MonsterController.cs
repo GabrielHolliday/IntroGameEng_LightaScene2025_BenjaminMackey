0a66ed847df907df26de203cba7741343df88579
Holliday <mackeybenjamin1@gmail.com>
2025-10-15 13:33:03 -0300

    why doesnt this work bruh

--- Assets/StudentScripts/MonsterController.cs
+++ Assets/StudentScripts/MonsterController.cs
@@ -1,5 +1,5 @@
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
+//using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using System;
 using System.Threading.Tasks;
@@ -33,40 +33,83 @@ public class MonsterController : MonoBehaviour
        
     }
 
+    static GameObject curNode = null;
     private async void moveCycle()
     {
         runMonsterCycle = true;
         while (runMonsterCycle == true)
         {
 
-            List<GameObject> possibleMovePoints = new List<GameObject>();
-            List<GameObject> bypassNodesToHit = new List<GameObject>();
+            List<List<GameObject>> possibleMovePoints = new List<List<GameObject>>();//saves as full paths, so each internal list is a list of nodes that make up a path
+            //List<GameObject> bypassNodesToHit = new List<GameObject>();
             for (int i = 0; i < nodes.Count; i++)
             {
-                GameObject passbackNode = monsterBody;
-                while (passbackNode != null)
+
+                if (nodes[i] == curNode) continue;
+                bool fT = true;
+                
+                GameObject passbackNode =  monsterBody; 
+                GameObject tNode = nodes[i];
+                GameObject internalPrevNode = null;
+                int emergencyCount = 0;
+                while (passbackNode != null && tNode != null)
                 {
+                    emergencyCount++;
                     RaycastHit hit;
-                    if (!Physics.Linecast(passbackNode.transform.position, nodes[i].transform.position, out hit))
+                    if (!Physics.Linecast(passbackNode.transform.position, tNode.transform.position, out hit))
                     {
                         break;
                     }
-                    if (hit.collider.gameObject.CompareTag("StopNode"))
+                    if (hit.transform.gameObject == tNode && hit.collider.gameObject.CompareTag("StopNode"))
                     {
+                        if(fT == true)
+                        {
+                            possibleMovePoints.Add(new List<GameObject>());
+                        }
+                        fT = false;
                         Debug.Log("Stop");
-                        possibleMovePoints.Add(nodes[i]);
+                        possibleMovePoints[possibleMovePoints.Count - 1].Add(hit.transform.gameObject);
                         passbackNode = null;
                     }
-                    else if (hit.collider.gameObject.CompareTag("BypassNode"))
+                    else if (hit.transform.gameObject == tNode && hit.transform.gameObject.CompareTag("BypassNode"))
                     {
+                        if (fT == true)
+                        {
+                            possibleMovePoints.Add(new List<GameObject>());
+                        }
+                        fT = false;
+                        internalPrevNode = hit.transform.gameObject;
                         Debug.Log("Bypass");
-                        passbackNode = hit.collider.gameObject;
+                        possibleMovePoints[possibleMovePoints.Count - 1].Add(hit.transform.gameObject);
+                        passbackNode = hit.transform.gameObject;
+                        //looking for another bypass node
+                        bool suc = false;
+                        for (int j = 0; j < nodes.Count; j++)
+                        {
+                            RaycastHit internalHit;
+                            if (!Physics.Linecast(passbackNode.transform.position, nodes[j].transform.position, out internalHit))
+                            {
+                                continue;
+                            }
+                            Debug.Log(possibleMovePoints[possibleMovePoints.Count - 1].Contains(internalHit.transform.gameObject));
+                            if ((internalHit.collider.gameObject.CompareTag("StopNode") | internalHit.collider.gameObject.CompareTag("BypassNode")) && internalHit.transform.gameObject != curNode && !possibleMovePoints[possibleMovePoints.Count - 1].Contains(internalHit.transform.gameObject))//fix here
+                            {
+                                suc = true; 
+                                tNode = internalHit.transform.gameObject;
+                            }
+                            
+                        }
+                        if(suc == false)
+                        {
+                            tNode = null;
+                        }
                     }
                     else
                     {
-                        Debug.Log(hit.collider.gameObject.name);
+                        //Debug.Log(hit.collider.gameObject.name);
                         passbackNode = null; //just in case
                     }
+                    if (emergencyCount > 5) break;
                 }
             }
 
@@ -75,16 +118,36 @@ public class MonsterController : MonoBehaviour
 
             await Task.Delay(rand.Next(5000, 10000));
 
-            //WIP !!!!!!!!!! CLEAR UP STUFF WITH THE BYPASS NODES SO THE MONSTER ACTUALLY FOLLOWS IT
 
-            Vector3 movePos = possibleMovePoints[rand.Next(0, possibleMovePoints.Count)].transform.position;
-            Vector3 startPos = monsterBody.transform.position;
+            List<GameObject> chosenPath = possibleMovePoints[rand.Next(0, possibleMovePoints.Count)];
+            /*
+            for (int i = 0; i < chosenPath.Count; i++)
+            {
+                Debug.Log(chosenPath[i].name);
+            }
+            */
+            for (int i = 0; i < chosenPath.Count; i++)
+            {
+                Vector3 endPos = chosenPath[i].transform.position;
+                Vector3 startPos = monsterBody.transform.position;
+
+                for (int j = 0; j < 1000/chosenPath.Count; j++) // change to use some kind of vector3.magnitude or something
+                {
+                    monsterBody.transform.position = Vector3.Lerp(startPos, endPos, (float)j / (float)(1000/chosenPath.Count));
+                    await Task.Delay(1);
+                }
+            }
+            curNode = chosenPath[chosenPath.Count - 1];
+
+            /*Vector3 movePos = possibleMovePoints[rand.Next(0, possibleMovePoints.Count)].transform.position;
+            
             
             for (int i = 0; i < bypassNodesToHit.Count; i++)
             {
                 await Task.Delay(1);
                 monsterBody.transform.position = Vector3.Lerp(startPos, movePos, (float)i / 1000);
             }
+            */
         }
     }
 
